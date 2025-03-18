2e5eb87f86ea8a5ddcc504bb9e1c33d480398fb8
minhthanh <autobot.tm19@gmail.com>
2025-03-18 07:40:44 +0700

    refactor: simplify TrashButton component and update button text

--- src/modules/maintenance/components/common/TrashButton.tsx
+++ src/modules/maintenance/components/common/TrashButton.tsx
@@ -1,28 +1,28 @@
-import { Button } from "@mui/material";
-import { GridDeleteIcon } from "@mui/x-data-grid";
-import React from "react";
+import { Button } from '@mui/material'
+import { GridDeleteIcon } from '@mui/x-data-grid'
+import React from 'react'
 interface Props {
-  onClick: (isDeleted: boolean) => void;
+	onClick: (isDeleted: boolean) => void
 }
 const TrashButton: React.FC<Props> = ({ onClick }) => {
-  const [isDeleted, setIsDeleted] = React.useState(true);
+	const [isDeleted, setIsDeleted] = React.useState(true)
 
-  const handleToggle = () => {
-    onClick(isDeleted);
-    setIsDeleted(!isDeleted);
-  };
+	const handleToggle = () => {
+		onClick(isDeleted)
+		setIsDeleted(!isDeleted)
+	}
 
-  return (
-    <Button
-      onClick={handleToggle}
-      endIcon={isDeleted ? <GridDeleteIcon /> : null}
-      variant="contained"
-      color="primary"
-      size="small"
-    >
-      {!isDeleted ? "Trở lại" : "Thùng rác"}
-    </Button>
-  );
-};
+	return (
+		<Button
+			onClick={handleToggle}
+			endIcon={isDeleted ? <GridDeleteIcon /> : null}
+			variant='contained'
+			color='primary'
+			size='small'
+		>
+			{!isDeleted ? 'Trở lại' : 'Gỡ bỏ'}
+		</Button>
+	)
+}
 
-export default TrashButton;
+export default TrashButton
