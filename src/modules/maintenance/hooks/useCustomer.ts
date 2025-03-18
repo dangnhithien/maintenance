14bac0a20610c83f70b867747f925347bc1a6f12
minhthanh <autobot.tm19@gmail.com>
2025-03-18 11:26:27 +0700

    refactor: rename 'Case' to 'Ticket' in relevant components and interfaces for consistency

--- src/modules/maintenance/hooks/useCustomer.ts
+++ src/modules/maintenance/hooks/useCustomer.ts
@@ -1,87 +1,98 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { AxiosError } from "axios";
-import customerApi from "../apis/customerApi";
-import { CreateCustomerDto } from "../datas/customer/CreateCustomerDto";
-import { DeleteCustomerDto } from "../datas/customer/DeleteCustomerDto";
-import { GetCustomerDto } from "../datas/customer/GetCustomerDto";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
+import customerApi from '../apis/customerApi'
+import { CreateCustomerDto } from '../datas/customer/CreateCustomerDto'
+import { DeleteCustomerDto } from '../datas/customer/DeleteCustomerDto'
+import { GetCustomerDto } from '../datas/customer/GetCustomerDto'
 
-const KEY = "Customers";
+const KEY = 'Customers'
 export const useCustomer = (initialParams?: GetCustomerDto) => {
-  const queryClient = useQueryClient();
+	const queryClient = useQueryClient()
 
-  // Fetch template checklists
-  const { data, isPending, isError, error } = useQuery({
-    queryKey: [KEY, initialParams], // Dynamic queryKey
-    queryFn: () => customerApi.get(initialParams).then((res) => res.result),
-    staleTime: 60000,
-    retry: (failureCount, error) => {
-      const axiosError = error as AxiosError;
-      if (
-        axiosError?.response?.status === 400 ||
-        axiosError?.response?.status === 401
-      ) {
-        return false; // Không retry với lỗi Bad Request hoặc Unauthorized
-      }
-      return failureCount < 3; // Retry với các lỗi khác
-    },
-  });
+	// Fetch template checklists
+	const { data, isPending, isError, error } = useQuery({
+		queryKey: [KEY, initialParams], // Dynamic queryKey
+		queryFn: () => customerApi.get(initialParams).then((res) => res.result),
+		staleTime: 60000,
+		retry: (failureCount, error) => {
+			const axiosError = error as AxiosError
+			if (
+				axiosError?.response?.status === 400 ||
+				axiosError?.response?.status === 401
+			) {
+				return false // Không retry với lỗi Bad Request hoặc Unauthorized
+			}
+			return failureCount < 3 // Retry với các lỗi khác
+		},
+	})
 
-  // Function to manually refetch with new params
-  const fetchCustomers = (newParams: GetCustomerDto) => {
-    queryClient.invalidateQueries({
-      queryKey: [KEY, newParams],
-    });
-  };
+	const getCustomerById = (id: string) => {
+		return useQuery({
+			queryKey: [KEY, id], // Cache theo ID
+			queryFn: () => customerApi.getById(id).then((res) => res.result),
+			enabled: !!id, // Chỉ fetch nếu có ID
+			staleTime: 60000,
+			retry: false,
+		})
+	}
 
-  // Create a new checklist
-  const createCustomer = useMutation({
-    mutationFn: (newData: CreateCustomerDto) => customerApi.post(newData),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [KEY] }); // Refresh list
-    },
-  });
+	// Function to manually refetch with new params
+	const fetchCustomers = (newParams: GetCustomerDto) => {
+		queryClient.invalidateQueries({
+			queryKey: [KEY, newParams],
+		})
+	}
 
-  // Update a checklist
-  const updateCustomer = useMutation({
-    mutationFn: ({
-      id,
-      updatedData,
-    }: {
-      id: string;
-      updatedData: CreateCustomerDto;
-    }) => customerApi.update(id, updatedData),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [KEY] });
-    },
-  });
+	// Create a new checklist
+	const createCustomer = useMutation({
+		mutationFn: (newData: CreateCustomerDto) => customerApi.post(newData),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: [KEY] }) // Refresh list
+		},
+	})
 
-  // Delete a checklist
-  const deleteCustomer = useMutation({
-    mutationFn: (data: DeleteCustomerDto) =>
-      customerApi.delete(data.isHardDeleted, data.ids),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [KEY] });
-    },
-  });
+	// Update a checklist
+	const updateCustomer = useMutation({
+		mutationFn: ({
+			id,
+			updatedData,
+		}: {
+			id: string
+			updatedData: CreateCustomerDto
+		}) => customerApi.update(id, updatedData),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: [KEY] })
+		},
+	})
 
-  const restoreChecklist = useMutation({
-    mutationFn: (ids: string[]) => customerApi.restore(ids),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [KEY] });
-    },
-  });
+	// Delete a checklist
+	const deleteCustomer = useMutation({
+		mutationFn: (data: DeleteCustomerDto) =>
+			customerApi.delete(data.isHardDeleted, data.ids),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: [KEY] })
+		},
+	})
 
-  return {
-    customers: data?.items || [],
-    totalCount: data?.totalCount || 0,
-    loading: isPending,
-    error: isError ? error?.message : null,
-    fetchCustomers, // Now accepts params
-    createCustomer: createCustomer.mutateAsync,
-    updateCustomer: updateCustomer.mutateAsync,
-    deleteCustomer: deleteCustomer.mutateAsync,
-    restoreCustomer: restoreChecklist.mutateAsync,
-  };
-};
+	const restoreChecklist = useMutation({
+		mutationFn: (ids: string[]) => customerApi.restore(ids),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: [KEY] })
+		},
+	})
 
-export default useCustomer;
+	return {
+		customers: data?.items || [],
+		totalCount: data?.totalCount || 0,
+		loading: isPending,
+		error: isError ? error?.message : null,
+		getCustomerById,
+		fetchCustomers, // Now accepts params
+		createCustomer: createCustomer.mutateAsync,
+		updateCustomer: updateCustomer.mutateAsync,
+		deleteCustomer: deleteCustomer.mutateAsync,
+		restoreCustomer: restoreChecklist.mutateAsync,
+	}
+}
+
+export default useCustomer
--- src/modules/maintenance/pages/case/CaseListPage.tsx
+++ src/modules/maintenance/pages/case/CaseListPage.tsx
@@ -1,26 +1,26 @@
-import CaseList from "@modules/maintenance/components/case/CaseList";
-import Wrapper from "@modules/maintenance/components/common/Wrapper";
-import { Helmet } from "react-helmet";
+import CaseList from '@modules/maintenance/components/case/CaseList'
+import Wrapper from '@modules/maintenance/components/common/Wrapper'
+import { Helmet } from 'react-helmet'
 
 const CaseListPage = () => {
-  const breadcrumbs = [
-    <span style={{ color: "#10428e", fontSize: "18px", fontWeight: 600 }}>
-      Danh sách khách hàng
-    </span>,
-  ];
+	const breadcrumbs = [
+		<span style={{ color: '#10428e', fontSize: '18px', fontWeight: 600 }}>
+			Danh sách khách hàng
+		</span>,
+	]
 
-  return (
-    <div>
-      <Helmet>
-        <title>Danh sách khách hàng</title>
-        <meta name="description" content="Danh sách các khách hàng" />
-      </Helmet>
+	return (
+		<>
+			<Helmet>
+				<title>Danh sách khách hàng</title>
+				<meta name='description' content='Danh sách các khách hàng' />
+			</Helmet>
 
-      <Wrapper title="Danh sách case">
-        <CaseList />
-      </Wrapper>
-    </div>
-  );
-};
+			<Wrapper title='Danh sách ticket'>
+				<CaseList />
+			</Wrapper>
+		</>
+	)
+}
 
-export default CaseListPage;
+export default CaseListPage
