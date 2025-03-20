17d11097c84111feb926b5a8fde4b8bb5a52b971
minhthanh <autobot.tm19@gmail.com>
2025-03-20 15:33:28 +0700

    feat: integrate LocalizationProvider for date handling in App and remove from DateFilter

--- src/components/DateFilter.tsx
+++ src/components/DateFilter.tsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react'
 import { Button, Box } from '@mui/material'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
 import { format } from 'date-fns'
-import { de } from 'date-fns/locale/de'
 
 export interface IFilterDate {
 	fromDate: Date
@@ -26,27 +23,25 @@ const DateFilter: React.FC<DateFilterProps> = ({ onFilter }) => {
 	}
 
 	return (
-		<LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={de}>
-			<Box display='flex' alignItems='center' gap={2}>
-				<DatePicker
-					label='Từ ngày'
-					value={startDate}
-					onChange={(newValue) => setStartDate(newValue)}
-					slotProps={{ textField: { size: 'small' } }}
-				/>
+		<Box display='flex' alignItems='center' gap={2}>
+			<DatePicker
+				label='Từ ngày'
+				value={startDate}
+				onChange={(newValue) => setStartDate(newValue)}
+				slotProps={{ textField: { size: 'small' } }}
+			/>
 
-				<DatePicker
-					label='Đến ngày'
-					value={endDate}
-					onChange={(newValue) => setEndDate(newValue)}
-					slotProps={{ textField: { size: 'small' } }}
-				/>
+			<DatePicker
+				label='Đến ngày'
+				value={endDate}
+				onChange={(newValue) => setEndDate(newValue)}
+				slotProps={{ textField: { size: 'small' } }}
+			/>
 
-				<Button variant='contained' color='primary' onClick={handleApply}>
-					Áp dụng
-				</Button>
-			</Box>
-		</LocalizationProvider>
+			<Button variant='contained' color='primary' onClick={handleApply}>
+				Áp dụng
+			</Button>
+		</Box>
 	)
 }
 
