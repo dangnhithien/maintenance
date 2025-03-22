f6eaa7af1c9384b62acd99c1212fc98a90df04a7
minhthanh <autobot.tm19@gmail.com>
2025-03-22 08:48:06 +0700

    refactor: remove unused maintenance index file and simplify component structure in dashboard and part pages

--- src/modules/maintenance/pages/dashBoard/DashBoardPage.tsx
+++ src/modules/maintenance/pages/dashBoard/DashBoardPage.tsx
@@ -1,16 +1,16 @@
-import Dashboard from "@modules/maintenance/components/dashboard/Dashboard";
-import { Helmet } from "react-helmet";
+import Dashboard from '@modules/maintenance/components/dashboard/Dashboard'
+import { Helmet } from 'react-helmet'
 
 const DashboardPage = () => {
-  return (
-    <div>
-      <Helmet>
-        <title>Danh sách thiết bị</title>
-        <meta name="description" content="Danh sách các thiết bị" />
-      </Helmet>
-      <Dashboard />
-    </div>
-  );
-};
+	return (
+		<>
+			<Helmet>
+				<title>Dashboard</title>
+				<meta name='description' content='Dashboard' />
+			</Helmet>
+			<Dashboard />
+		</>
+	)
+}
 
-export default DashboardPage;
+export default DashboardPage
